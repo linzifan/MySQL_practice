68fb3018946cb695fe24da1ae8fc5009ab6fa055
linzifan <zl2364@columbia.edu>
2015-04-14 12:07:39 -0400

    join_union

--- join_union.sql
+++ join_union.sql
@@ -1,3 +1,5 @@
+# A JOIN clause is used to combine rows from two or more tables, based on a common field between them.
+
 USE test;
 
 DROP TABLE IF EXISTS department;
@@ -17,7 +19,7 @@ CREATE TABLE student(
 
 INSERT INTO department VALUES (1, 'Statistics'), (2, 'Math'), (3, 'Economics'), (4, 'Physics');
 INSERT INTO student VALUES (1, 'Allen', 2, 97), (2, 'Bob', 2, 78), (3, 'Chris', 3, 87),
-(4, 'David', 1, 99), (5, 'Eric', 3, 87), (6, 'Frank', 1, 92), (7, 'Gary', 3, 87), (8, 'Harry', 1, 76);
+(4, 'David', 1, 99), (5, 'Eric', 3, 87), (6, 'Frank', 1, 92), (7, 'Gary', 3, 87), (8, 'Harry', 5, 76);
 
 SELECT * FROM department;
 #+---------------+-----------------+
@@ -39,10 +41,10 @@ SELECT * FROM student;
 #|          5 | Eric         |             3 |    87 |
 #|          6 | Frank        |             1 |    92 |
 #|          7 | Gary         |             3 |    87 |
-#|          8 | Harry        |             1 |    76 |
+#|          8 | Harry        |             5 |    76 |
 #+------------+--------------+---------------+-------+
 
-# INNER JOIN Returns all rows for which there is at least one match in BOTH tables. 
+# INNER JOIN Returns all rows from multiple tables where the join condition is met. 
 # This is the default type of join if no specific JOIN type is specified.
 SELECT * FROM department INNER JOIN student ON department.department_id = student.department_id;
 #+---------------+-----------------+------------+--------------+---------------+-------+
@@ -55,10 +57,13 @@ SELECT * FROM department INNER JOIN student ON department.department_id = studen
 #|             3 | Economics       |          5 | Eric         |             3 |    87 |
 #|             1 | Statistics      |          6 | Frank        |             1 |    92 |
 #|             3 | Economics       |          7 | Gary         |             3 |    87 |
-#|             1 | Statistics      |          8 | Harry        |             1 |    76 |
 #+---------------+-----------------+------------+--------------+---------------+-------+
-#8 rows in set
+#7 rows in set
+# No department_id = 4 (in department). No department_id = 5 (in student).
 
+
+# LEFT JOIN Returns all rows from the left table, with the matching rows in the right table.
+# The result is NULL in the right side when there is no match.
 SELECT * FROM department LEFT JOIN student ON department.department_id = student.department_id;
 #+---------------+-----------------+------------+--------------+---------------+-------+
 #| department_id | department_name | student_id | studnet_name | department_id | score |
@@ -70,27 +75,37 @@ SELECT * FROM department LEFT JOIN student ON department.department_id = student
 #|             3 | Economics       |          5 | Eric         |             3 |    87 |
 #|             1 | Statistics      |          6 | Frank        |             1 |    92 |
 #|             3 | Economics       |          7 | Gary         |             3 |    87 |
-#|             1 | Statistics      |          8 | Harry        |             1 |    76 |
 #|             4 | Physics         |       NULL | NULL         |          NULL |  NULL |
 #+---------------+-----------------+------------+--------------+---------------+-------+
-#9 rows in set
+#8 rows in set
+# No department_id = 5 (in student).
 
+
+# RIGHT JOIN Returns all rows from the right table, with the matching rows in the left table.
+# The result is NULL in the left side when there is no match.
 SELECT * FROM department RIGHT JOIN student ON department.department_id = student.department_id;
 #+---------------+-----------------+------------+--------------+---------------+-------+
 #| department_id | department_name | student_id | studnet_name | department_id | score |
 #+---------------+-----------------+------------+--------------+---------------+-------+
 #|             1 | Statistics      |          4 | David        |             1 |    99 |
 #|             1 | Statistics      |          6 | Frank        |             1 |    92 |
-#|             1 | Statistics      |          8 | Harry        |             1 |    76 |
 #|             2 | Math            |          1 | Allen        |             2 |    97 |
 #|             2 | Math            |          2 | Bob          |             2 |    78 |
 #|             3 | Economics       |          3 | Chris        |             3 |    87 |
 #|             3 | Economics       |          5 | Eric         |             3 |    87 |
 #|             3 | Economics       |          7 | Gary         |             3 |    87 |
+#|          NULL | NULL            |          8 | Harry        |             5 |    76 |
 #+---------------+-----------------+------------+--------------+---------------+-------+
 #8 rows in set
+# No department_id = 4 (in department).
 
-SELECT * FROM department FULL JOIN student;
+
+# CROSS JOIN Returns the Cartesian product of rows from tables
+# It will produce rows which combine each row from the first table with each row from the second table
+# The following three have the same result (MySQL does not support FULL JOIN)
+SELECT * FROM department CROSS JOIN student;
+SELECT * FROM department, student;
+SELECT department_name, student_id FROM department FULL JOIN student;
 #+---------------+-----------------+------------+--------------+---------------+-------+
 #| department_id | department_name | student_id | studnet_name | department_id | score |
 #+---------------+-----------------+------------+--------------+---------------+-------+
@@ -122,10 +137,10 @@ SELECT * FROM department FULL JOIN student;
 #|             2 | Math            |          7 | Gary         |             3 |    87 |
 #|             3 | Economics       |          7 | Gary         |             3 |    87 |
 #|             4 | Physics         |          7 | Gary         |             3 |    87 |
-#|             1 | Statistics      |          8 | Harry        |             1 |    76 |
-#|             2 | Math            |          8 | Harry        |             1 |    76 |
-#|             3 | Economics       |          8 | Harry        |             1 |    76 |
-#|             4 | Physics         |          8 | Harry        |             1 |    76 |
+#|             1 | Statistics      |          8 | Harry        |             5 |    76 |
+#|             2 | Math            |          8 | Harry        |             5 |    76 |
+#|             3 | Economics       |          8 | Harry        |             5 |    76 |
+#|             4 | Physics         |          8 | Harry        |             5 |    76 |
 #+---------------+-----------------+------------+--------------+---------------+-------+
 #32 rows in set
 
@@ -140,12 +155,34 @@ SELECT * FROM department CROSS JOIN student ON department.department_id = studen
 #|             3 | Economics       |          5 | Eric         |             3 |    87 |
 #|             1 | Statistics      |          6 | Frank        |             1 |    92 |
 #|             3 | Economics       |          7 | Gary         |             3 |    87 |
-#|             1 | Statistics      |          8 | Harry        |             1 |    76 |
 #+---------------+-----------------+------------+--------------+---------------+-------+
-#8 rows in set
+#7 rows in set
+
+
+# FULL OUTER JOIN combines the result of both LEFT and RIGHT joins.
+# MySQL does not support FULL OUTER JOIN. 
+# A solution is to use UNION ALL to connect a LEFT JOIN and a RIGHT JOIN.
+SELECT * FROM department LEFT JOIN student ON department.department_id = student.department_id
+UNION
+SELECT * FROM department RIGHT JOIN student ON department.department_id = student.department_id;
+#+---------------+-----------------+------------+--------------+---------------+-------+
+#| department_id | department_name | student_id | studnet_name | department_id | score |
+#+---------------+-----------------+------------+--------------+---------------+-------+
+#|             2 | Math            |          1 | Allen        |             2 |    97 |
+#|             2 | Math            |          2 | Bob          |             2 |    78 |
+#|             3 | Economics       |          3 | Chris        |             3 |    87 |
+#|             1 | Statistics      |          4 | David        |             1 |    99 |
+#|             3 | Economics       |          5 | Eric         |             3 |    87 |
+#|             1 | Statistics      |          6 | Frank        |             1 |    92 |
+#|             3 | Economics       |          7 | Gary         |             3 |    87 |
+#|             4 | Physics         |       NULL | NULL         |          NULL |  NULL |
+#|          NULL | NULL            |          8 | Harry        |             5 |    76 |
+#+---------------+-----------------+------------+--------------+---------------+-------+
+#9 rows in set
 
 
 # What does UNION do? What is the difference between UNION and UNION ALL?
+# UNION will remove duplicate rows.
 SELECT department_id FROM department UNION SELECT department_id FROM student;
 #+---------------+
 #| department_id |
